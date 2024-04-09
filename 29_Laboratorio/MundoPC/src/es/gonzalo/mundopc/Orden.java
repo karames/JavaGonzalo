c29444c61106bb8a0241a2f5b1681f232efc7e7e
Karames <karames@gmail.com>
2024-04-09 14:00:10 +0200

    laboratorio terminado

--- 29_Laboratorio/MundoPC/src/es/gonzalo/mundopc/Orden.java
+++ 29_Laboratorio/MundoPC/src/es/gonzalo/mundopc/Orden.java
@@ -1,5 +1,31 @@
 package es.gonzalo.mundopc;
 
 public class Orden {
+    private int idOrden;
+    private Computadora computadoras[];
+    private static int contadoresOrdenes;
+    private static int contadorComputadoras;
+    private static final int MAX_COMPUTADORAS = 10;
+
+    public Orden() {
+        idOrden = ++contadoresOrdenes;
+        computadoras = new Computadora[MAX_COMPUTADORAS];
+    }
+
+    public void agregarComputadora(Computadora computadora) {
+        if (contadorComputadoras < MAX_COMPUTADORAS) {
+            computadoras[contadorComputadoras++] = computadora;
+        } else {
+            System.out.println("se ha superado el maximo de productos: " + MAX_COMPUTADORAS);
+        }
+    }
+
+    public void mostrarOrden() {
+        System.out.println("Orden # " + idOrden + " #");
+        System.out.println("computadoras de esta orden");
+        for (int i = 0; i < contadorComputadoras; i++) {
+            System.out.println(computadoras[i]);
+        }
+    }
 
 }
--- 29_Laboratorio/MundoPC/src/mundopc/MundoPC.java
+++ 29_Laboratorio/MundoPC/src/mundopc/MundoPC.java
@@ -1,7 +1,39 @@
 package mundopc;
 
+import es.gonzalo.mundopc.Computadora;
+import es.gonzalo.mundopc.Monitor;
+import es.gonzalo.mundopc.Orden;
+import es.gonzalo.mundopc.Raton;
+import es.gonzalo.mundopc.Teclado;
+
 public class MundoPC {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // CREAR COMPUTADORA TOSHIBA
+        Monitor monitorToshiba = new Monitor("Toshiba", 13);
+        Teclado tecladoToshiba = new Teclado("bluetooth", "Toshiba");
+        Raton ratonToshiba = new Raton("bluetooth", "Toshiba");
+        Computadora computadoraToshiba = new Computadora(
+                "Computadora Toshiba", monitorToshiba, tecladoToshiba, ratonToshiba);
+
+        // crear computadora dell
+        Monitor monitorDell = new Monitor("Dell", 16);
+        Teclado tecladoDell = new Teclado("USB", "Dell");
+        Raton ratonDell = new Raton("USB", "Dell");
+        Computadora computadoraDell = new Computadora(
+                "Computadora Dell", monitorDell, tecladoDell, ratonDell);
+
+        // CREAR COMPUTADOORA PEERSDONALIZADA
+        Computadora computadoraPersonalizada = new Computadora(
+                "Computadora Personalizada", monitorToshiba, tecladoDell, ratonDell);
+
+        // crear orden 1
+        Orden orden1 = new Orden();
+        orden1.agregarComputadora(computadoraToshiba);
+        orden1.agregarComputadora(computadoraDell);
+        orden1.agregarComputadora(computadoraPersonalizada);
+
+        orden1.mostrarOrden();
+
     }
+
 }
